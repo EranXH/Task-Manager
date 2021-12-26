8f9d3d418e394b12dbbbafb7ab52a0965c837da0
EranXH <82323696+EranXH@users.noreply.github.com>
2021-12-27 00:47:10 +0200

    Delete Task Manager/Code directory

--- Task Manager/Code/Widgets/ComplexWidgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .CheckListItemWidget import CheckListItemWidget
-from .CheckListWidget import CheckListWidget
-from .SelectionAddButtonWidget import SelectionAddButtonWidget
-from .SelectionWidget import SelectionWidget
-from .TasksListWidget.TasksListWidget import TasksListWidget
-from .TreeItemWidget import TreeItemWidget
-from .TreeWidget import TreeWidget
